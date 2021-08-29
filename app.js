8001c2c12864f156274aa6491e7bdc0a8822e1f9
bowdiedulcano <37744689+bowdiedulcano@users.noreply.github.com>
2021-08-29 14:12:51 +0100

    Add flow to Pipedream

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ const dotenv = require('dotenv').config();
 const YouTubeNotifier = require('youtube-notification');
 const axios = require('axios');
 
-var myLogger = function (req, res, next) {
-  console.log('LOGGED')
-  next()
-}
-//notifier.server.use(myLogger)
-
 const notifier = new YouTubeNotifier({
   hubCallback: process.env.YOUTUBE_CALLBACK_URL,
   port: process.env.PORT,
@@ -17,8 +11,9 @@ const notifier = new YouTubeNotifier({
 notifier.setup();
 
 notifier.on('subscribe', async data => {
-  console.log('Subscribed');
-  console.log(data);
+//  console.log('Subscribed');
+//  console.log(data);
+// Add message to my discord that the subscriptions have been sucessful
   var discord_webhook
   if (process.env.MESSAGE_DISCORD === 'true') {
     const discord_content = 'Subscription to Channel ID:' + data.channel
@@ -36,27 +31,74 @@ notifier.on('subscribe', async data => {
   }
 });
 
-notifier.on('unsubscribe', data => {
-  console.log('Unsubscribed');
-  console.log(data);
+notifier.on('unsubscribe', async data => {
+//  console.log('Unsubscribed');
+//  console.log(data);
+// Add message to my discord that an unsubscribe have been sucessful
+var discord_webhook
+// if (process.env.MESSAGE_DISCORD === 'true') {
+  const discord_content = 'Unsubscribe received to Channel ID:' + data.channel
+  discord_webhook = await axios({
+    method: "POST",
+    url: process.env.DISCORD_WEBHOOK,
+    headers: {
+      "Content-Type": 'application/json',
+    },
+    data: {
+      "username": `Youtube PubSub Test`,
+      "content": discord_content,
+    }
+  })
+// }
 });
 
-notifier.on('denied', data => {
+notifier.on('denied', async data => {
   console.log('Denied');
   console.log(data);
-});
+  var discord_webhook
+  // if (process.env.MESSAGE_DISCORD === 'true') {
+    const discord_content = 'Denied message created. Check logs.'
+    discord_webhook = await axios({
+      method: "POST",
+      url: process.env.DISCORD_WEBHOOK,
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      data: {
+        "username": `Youtube PubSub Test`,
+        "content": discord_content,
+      }
+    })
+  // }
+  });
 
-notifier.on('notified', data => {
+notifier.on('notified', async data => {
   console.log('New Video');
   console.log(data);
+  var discord_webhook
+  let youtube_title = data.video.title.trim().replace(/[-[/\]"\\]/g, '\\\$&')
+  discord_webhook = await axios({
+    method: "POST",
+    url: process.env.PIPEDREAM_WEBHOOK,
+    headers: {
+      "Content-Type": 'application/json',
+    },
+    data: {
+      "youtube_channel_id": data.video.id,
+      "youtube_id": data.channel.id,
+      "youtube_title": youtube_title,
+    }
+  })
 });
 
 console.log('Listening on port ' + process.env.PORT);
 
+// Add root directory to server so it can be kept alive
 notifier.server.get('/', function (req, res) {
-  res.status(200).send('The webiste is alive.')
+  res.status(200).send('The website is alive.')
 })
 
+// Add test directory to server which will send a message to discord
 notifier.server.get('/test_discord', async function (req, res) {
   res.status(200).send('Sending message to discord if permitted.')
   var discord_webhook
