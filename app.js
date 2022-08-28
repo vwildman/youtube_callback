6e448892dd5849efcd36733af24a05f694929cd0
vwildman <37744689+vwildman@users.noreply.github.com>
2022-08-28 09:06:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -134,16 +134,16 @@ notifier.server.get('/test_discord', async function (req, res) {
 })
 
 // Twitch Subsciptions
-var twitch_subs_array = process.env.TWITCH_SUBS.split(",");
+/* var twitch_subs_array = process.env.TWITCH_SUBS.split(",");
 for (twitch_sub of twitch_subs_array) {
   tes.subscribe('stream.online', {
     broadcaster_user_id: twitch_sub
   });
   delay(2000);
-} 
+} */
 
 // List all Twitch subsciptions
-/*tes.getSubscriptionsByType('stream.online').then(data => {
+/* tes.getSubscriptionsByType('stream.online').then(data => {
   console.log(JSON.stringify(data,0,2));
 }); */
 
